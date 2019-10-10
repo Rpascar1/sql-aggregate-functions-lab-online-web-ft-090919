868521c8eb00ac58cb8b6eacc624a85e05533757
Richard Pascarelli <rpavesi84@gmail.com>
2019-10-10 03:35:01 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,11 +1,10 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  
-  "SELECT MAX(gpa) FROM students;"
+  SELECT MAX(gpa) FROM 
 end
 
-  
+  SELECT AVG(net_worth) AS average_net_worth FROM cats;
 end
 
 def average_student_gpa
