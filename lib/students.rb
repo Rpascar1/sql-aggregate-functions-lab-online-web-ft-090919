6e7f4f6f854459ef0d0c50a68542d6a0eecf9398
Richard Pascarelli <rpavesi84@gmail.com>
2019-10-10 03:46:28 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-"SELECT * tardies FROM students WHERE grade"
+"SELECT * tardies FROM students WHERE grade > 10"
 end
 
 def average_gpa_for_9th_grade
